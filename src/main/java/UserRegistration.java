19471b47c2eef4961cbd435f7e6114ffd4137f2c
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-04 16:49:49 +0530

    ADDED: Method to validate first name of user

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,5 +1,16 @@
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    //PATTERN FOR FIRST NAME
+    public String firstNamePattern="^[A-Z][a-zA-z]{2,}$";
+
+    //METHOD TO VALIDATE FIRST NAME OF USER
+    public boolean validateFirstName(String firstName) {
+        return (Pattern.matches(firstNamePattern,firstName));
+    }
+    //MAIN METHOD
     public static void main(String[] args) {
         System.out.println("/**************************/ WELCOME TO USER REGISTRATION /**************************/");
+
     }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -1,3 +1,20 @@
-public class UserRegistrationTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+
+public class UserRegistrationTest {
+    //TEST CASE FOR VALID FIRST NAME
+    @Test
+    public void givenFirstName_WhenValid_ThenReturn() {
+        UserRegistration userRegistration=new UserRegistration();
+        boolean result = userRegistration.validateFirstName("Nilesh");
+        Assert.assertTrue(result);
+    }
+    //TEST CASE FOR INVALID FIRST NAME
+    @Test
+    public void givenFirstName_WhenInValid_ThenReturn() {
+        UserRegistration userRegistration=new UserRegistration();
+        boolean result=userRegistration.validateFirstName("nilesh");
+        Assert.assertFalse(result);
+    }
 }
